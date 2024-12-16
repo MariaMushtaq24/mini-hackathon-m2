373da970da86a844f10cef8c97df2bd45ae33fb3
MariaMushtaq24 <mariaashaikh25@gmail.com>
2024-12-16 17:36:20 +0500

    Add files via upload

---
+++ src/app/components/ContactUs.tsx
@@ -0,0 +1,8 @@
+export default function ContactUs(){
+    return(
+        <div>
+        <h1 className="flex align-center text-black font-semi-bold bg-gray-400">Contact Us</h1>
+        <p>Leave a message on any of our social media platforms.</p>
+        </div>
+    )
+};
